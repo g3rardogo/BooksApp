871be8ff130ed174133c38a143ec710285535e6f
Gerardo Ramirez <gerardogo145@gmail.com>
2021-01-23 23:56:22 +0100

    Creando clase Book

---
+++ app/src/main/java/com/books/app/model/Book.kt
@@ -0,0 +1,11 @@
+package com.books.app.model
+
+class Book {
+    var title = ""
+    var author = ""
+    var description = ""
+    var website = ""
+    var price = 0.0
+    var category = ""
+    var image = ""
+}
