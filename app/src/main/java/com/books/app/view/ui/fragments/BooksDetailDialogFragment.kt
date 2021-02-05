fcc3abbfae50ef86c6507893d4ff0376617bac05
Gerardo Ramirez <gerardogo145@gmail.com>
2021-02-05 19:00:46 +0100

    Ocultando el Action Bar de todos los activities

--- app/src/main/java/com/books/app/view/ui/fragments/BooksDetailDialogFragment.kt
+++ app/src/main/java/com/books/app/view/ui/fragments/BooksDetailDialogFragment.kt
@@ -1,12 +1,14 @@
 package com.books.app.view.ui.fragments
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.books.app.R
 import com.books.app.model.Book
@@ -46,6 +48,11 @@ class BooksDetailDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        toolbarBook.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarBook.setTitleTextColor(Color.WHITE)
+        toolbarBook.setOnClickListener{
+            dismiss()
+        }
         val book = arguments?.getSerializable("book") as Book
         Glide.with(ivDetailBookImage.context)
             .load(book.image)
