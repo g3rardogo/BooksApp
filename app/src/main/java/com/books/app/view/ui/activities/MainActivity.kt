6ef6add4b83ca1d8605ffab1d2abf2c2d29a28a5
Gerardo Ramirez <gerardogo145@gmail.com>
2021-02-14 23:28:46 +0100

    Separando el menu de categorias hacia otro fragmento y mostrandolo en la pantalla principal

--- app/src/main/java/com/books/app/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/books/app/view/ui/activities/MainActivity.kt
@@ -3,11 +3,20 @@ package com.books.app.view.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.books.app.R
+import com.books.app.view.ui.fragments.BooksFragment
+import com.books.app.view.ui.fragments.CategoriesMenuFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var menuFragment = CategoriesMenuFragment()
+        var mainFragment = BooksFragment()
+
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.llMenu, menuFragment, menuFragment.tag)
+                .commit()
 
         supportActionBar?.hide()
     }
