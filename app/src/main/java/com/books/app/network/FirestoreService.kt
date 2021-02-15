43eb848918e641ccab5b5dd1a8c5813d21911e66
Gerardo Ramirez <gerardogo145@gmail.com>
2021-02-15 18:11:43 +0100

    Creando funcion para obtener la coleccion de categorias

--- app/src/main/java/com/books/app/network/FirestoreService.kt
+++ app/src/main/java/com/books/app/network/FirestoreService.kt
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 const val BOOKS_COLLECTION_NAME ="books"
+const val CATEGORY_COLLECTION_NAME = "categories"
 
 class FirestoreService {
     val firebaseFirestore = FirebaseFirestore.getInstance()
@@ -22,6 +23,19 @@ class FirestoreService {
                 for (doc in result){
                     val list = result.toObjects(Book::class.java)
                     callback.onSuccess(list)
+                    break
+                }
+            }
+    }
+
+    fun getCategories(callback: Callback<List<Category>>){
+        firebaseFirestore.collection(CATEGORY_COLLECTION_NAME)
+            .orderBy("name")
+            .get()
+            .addOnSuccessListener { result ->
+                for (doc in result){
+                    val listCategories = result.toObjects(Category::class.java)
+                    callback.onSuccess(listCategories)
                     break
                 }
             }
